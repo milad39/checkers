29c0b1366fcefcab3ef5bd568c48f3ffd07cf17c
milad39 <98696868+milad39@users.noreply.github.com>
2022-01-30 06:26:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php
 
+<html>
+<body style="line-height: 10px;">
+<?php
 
+for ($i=0;$i<50;$i++){
+	for ($j=0;$j<20;$j++)
+	{
+		$color=random_color();
+		$i%2==0?$m=2:$m=3;
+
+		?>
+		<div style="border-radius:2px;padding:2;margin:0;display: inline-block;height:50px;width:50px;background-color:<?php echo $color; ?>">&nbsp;</div>
+		<?php
+	}
+	echo "<hr>";
+}
+
+function random_color_part() {
+    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+}
+
+function random_color() {
+    return random_color_part() . random_color_part() . random_color_part();
+}
+
+
+?>
+</body>
+</html>
 
 
 ?>
